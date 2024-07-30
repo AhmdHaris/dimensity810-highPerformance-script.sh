f91e1b9df34a5b9a18896293c81d2cf47705311e
Haris <161436891+AhmdHaris@users.noreply.github.com>
2024-07-30 20:57:05 +0700

    update script: Add more Warning, Add Full enable GED Parameter

--- dimensity810-highPerf-script.sh
+++ dimensity810-highPerf-script.sh
@@ -17,6 +17,42 @@ echo
 sleep 3
 print_warning "Please Do With Your Own Risk !"
 echo
+sleep 1
+print_warning "Please Execute The Script With Root Permission!"
+sleep 1
+echo 
+echo "[ - ] If you find an error, please report it in Issue"
+sleep 1
+echo
+
+echo "Enabling GED Modules for Mediatek Dimensity..."
+sleep 1
+echo
+# Function to set GED module parameter
+set_ged_param() {
+  echo "$1" > "/sys/module/ged/parameters/$2"
+}
+
+# GED Modules
+set_ged_param 1 gx_game_mode
+set_ged_param 1 gx_force_cpu_boost
+set_ged_param 1 boost_amp
+set_ged_param 1 boost_extra
+set_ged_param 1 boost_gpu_enable
+set_ged_param 1 enable_cpu_boost
+set_ged_param 1 enable_gpu_boost
+set_ged_param 1 enable_game_self_frc_detect
+set_ged_param 10 gpu_idle
+set_ged_param 100 cpu_boost_policy
+set_ged_param 0 ged_force_mdp_enable
+set_ged_param 1 ged_boost_enable
+set_ged_param 100 ged_smart_boost
+set_ged_param 1 gx_frc_mode
+set_ged_param 1 gx_boost_on
+
+echo "GED Modules enabled"
+sleep 1
+echo
 
 # Function to set CPU governor
 set_governor() {
@@ -33,7 +69,8 @@ set_governor performance
 
 # Verify governor settings
 sleep 1
-echo "Checking governor settings..."
+echo "Checking governor schedule..."
 echo "core 0-5: $(cat /sys/devices/system/cpu/cpufreq/policy0/scaling_governor)"
+echo
 sleep 1
 echo "core 6-7: $(cat /sys/devices/system/cpu/cpufreq/policy6/scaling_governor)"
